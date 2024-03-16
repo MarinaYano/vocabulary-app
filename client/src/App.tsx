aa96c5318a9b33a322f0643ac07b64802fb137f4
MarinaYano <myano2621@gmail.com>
2024-03-15 17:17:38 -0700

    fix path errors in shadcn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,23 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
-
+import Layout from './pages/Layout'
+import Home from './pages/Home'
+import { ThemeProvider } from "@/components/theme-provider"
+import Save from './pages/Save'
 
 function App() {
-  
 
   return (
-    <></>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Router>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path='/save' element={<Save />} />
+          </Route>
+        </Routes>
+      </Router>
+    </ThemeProvider>
   )
 }
 
