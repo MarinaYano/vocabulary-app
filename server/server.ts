8d73428ab622897d44372b4e12e0ef5c94439f4d
MarinaYano <myano2621@gmail.com>
2024-03-20 23:59:29 -0700

    connect to MongoDB

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import dotenv from "dotenv"
 
 import authRoutes from "./routes/auth.routes";
+import connectToMongoDB from "./db/connectToMongoDB";
 
+dotenv.config()
 const app: express.Express = express();
-const port = 8000;
+const port = process.env.PORT || 5000;
 
 app.get("/", (req: express.Request, res: express.Response) => {
   res.send("Hello, Node");
@@ -12,5 +15,6 @@ app.get("/", (req: express.Request, res: express.Response) => {
 app.use("/api/auth", authRoutes)
 
 app.listen(port, () => {
+  connectToMongoDB()
   console.log(`listening to port ${port}`);
 });
